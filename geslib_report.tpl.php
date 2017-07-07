dcd1686f23d14e64eb456569f09edd6866069575
sramos <sramos@sitiodistinto.net>
2017-07-07 19:48:46 +0200

    Informe de estado

---
+++ geslib_report.tpl.php
@@ -0,0 +1,28 @@
+
+<table>
+  <tr>
+    <th><?php print t("Geslib File"); ?></th>
+    <th><?php print t("Element"); ?></th>
+    <th><?php print t("Starting Date"); ?></th>
+    <th><?php print t("Ending Date"); ?></th>
+    <th><?php print "#"; ?></th>
+    <th><?php print t("Status"); ?></th>
+    <th>&nbsp;</th>
+  </tr>
+
+<?php
+  global $base_path;
+  foreach($items as $record) {
+    print '<tr id="geslib_file_' . $record->id . '">';
+    print '  <td>' . $record->imported_file . '</td>';
+    print '  <td>' . $record->component . '</td>';
+    print '  <td>' . $record->start_date . '</td>';
+    print '  <td>' . $record->end_date . '</td>';
+    print '  <td>' . $record->count . '</td>';
+    print '  <td>' . $record->status . '</td>';
+    print '  <td><a href="' . $base_path . 'geslib/delete_log/' . $record->id . '">Delete</a></td>';
+    print '</tr>';
+  }
+?>
+
+</table>
