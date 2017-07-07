38bcf98e3b7b850032869ad1604d4ca574bc044b
sramos <sramos@sitiodistinto.net>
2017-07-07 20:16:18 +0200

    Preparando escritura de nodos

---
+++ lib/GeslibWriter.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Define how to interact with geslib files
+ * Info: http://www.unleashed-technologies.com/blog/2010/07/16/drupal-6-inserting-updating-nodes-programmatically
+ */
+
+include_once dirname(__FILE__) . '/Encoding.php';
+
+class GeslibWriter {
+
+  function __construct() {
+  }
+
+}
