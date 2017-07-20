be88ba93f92a2a3ddbfe8126d731ead40a00d2eb
sramos <sramos@sitiodistinto.net>
2017-07-20 20:08:39 +0200

    Guardado de campos segun mapeo y preparacion de guardado de referencias

--- lib/GeslibWriter.php
+++ lib/GeslibWriter.php
@@ -62,7 +62,6 @@ class GeslibWriter {
   * Write element to database
   */
   function process_elements() {
-    $drupal_node_type = $this->node_type;
     # If true, link existing nodes with same title to geslib. If false, create node if there is no linked yet
     $use_existing_nodes = ($this->elements_type == "category");
 
@@ -70,7 +69,7 @@ class GeslibWriter {
     $total_counter = 0;
     # Loop elements and send it to apropiate action
     GeslibCommon::vprint(t("Importing")." ".$this->elements_type, 1);
-    foreach ($elements as $object_id => $object) {
+    foreach ($this->elements as $object_id => $object) {
       if ($counter == 1000) {
         # Not sure if this clear internal node
         #$this->flush_cache();
@@ -82,18 +81,18 @@ class GeslibWriter {
           $object["action"] = "M";
         }
         # Create or update node and use it to modify parameters (linked or defined)
-        $node = $this->update_object($drupal_node_type, $object_id, $object, $use_existing_nodes);
+        $node = $this->update_object($object_id, $object, $use_existing_nodes);
         if ( $node ) {
           # Update node attributes, relationships and taxonomy terms
           $this->update_attributes($node, $object);
-          $this->update_relationships($node, $drupal_node_type, $object);
+          $this->update_relationships($node, $object);
           // Remove object from memory
           $node = NULL;
         } else {
-          GeslibCommon::vprint(t("Node @type with GeslibID @gid doesn't exist", array('@type' => $drupal_node_type, '@gid' => $object_id)), 0);
+          GeslibCommon::vprint(t("Node @type with GeslibID @gid doesn't exist", array('@type' => $this->node_type, '@gid' => $object_id)), 0);
         }
       } else {
-        $this->delete_object($drupal_node_type, $object_id);
+        $this->delete_object($this->node_type, $object_id);
       }
       $counter += 1;
       $total_counter += 1;
@@ -105,7 +104,6 @@ class GeslibWriter {
   * Write products (books and other) to database
   */
   function process_products() {
-    $drupal_node_type = $this->node_type;
     # If true, link existing nodes with same title to geslib. If false, create node if there is no linked yet
     $use_existing_nodes = ($this->elements_type == "category");
 
@@ -168,10 +166,10 @@ class GeslibWriter {
   * @param use_existing_nodes
   *   If true, link existing nodes with same title to geslib. If false, create node if there is no linked yet
   */
-  function update_object($node_type, $object_id, &$object, $use_existing_nodes) {
+  function update_object($object_id, &$object, $use_existing_nodes) {
     $new_element = false;
     # Get node if exists
-    $node = $this->get_node_by_gid($object_id, $node_type);
+    $node = $this->get_node_by_gid($object_id, $this->node_type);
     # If node exists, only gets authorization for update
     if ( $node ) {
     # Return NULL if doesn't exists and there is no ADD or MODIFY action
@@ -192,11 +190,6 @@ class GeslibWriter {
     # Title
     if (array_key_exists('title', $object)) {
       $node->title = $object['title'];
-    }
-    # Body
-    if (array_key_exists('body', $object)) {
-      // Format 5: plaintext
-      $this->update_attribute($node, 'body', $object['body'], 5);
     }
 
     # Check that node is ready and save it
@@ -241,24 +234,33 @@ class GeslibWriter {
   function update_attributes($node, $object_data) {
     # Elimina los atributos restringidos
     $bad_keys = array('action','title','type');
-    $good_data = array_diff_key($object_data,array_flip($bad_keys));
+    $good_data = array_diff_key($object_data['attribute'],array_flip($bad_keys));
+    # Recoge si el nodo ha cambiado
+    $changed = false;
+
     # Recorre el resto de atributos actualizando la info
     foreach ($good_data as $attr_name => $attr_value) {
-      $this->update_attribute($node, $attr_name, $attr_value, 5);
+      // Format 5: plaintext
+      if ($this->change_attribute($node, $attr_name, $attr_value, 5)) {
+        $changed = true;
+      }
     }
-    # Check that node is ready and save it
-    if ($node = node_submit($node)) {
-      node_save($node);
-      GeslibCommon::vprint(t("Node")." '".$node->title."' (NID:".$node->nid."/GID:".$object_id.") ".t("attributes updated correctly"), 2);
-      return $node;
-    } else {
-      GeslibCommon::vprint(t("Node")." '".$node->title."' (NID:".$node->nid."/GID:".$object_id.") ".t("attributes processed incorrectly"), 0);
-      return NULL;
+    # Check that node changed
+    if ($changed) {
+      # If and is ready and it can be saved
+      if ($node = node_submit($node)) {
+        node_save($node);
+        GeslibCommon::vprint(t("Node")." '".$node->title."' (NID:".$node->nid."/GID:".$object_id.") ".t("attributes updated correctly"), 2);
+        return $node;
+      } else {
+        GeslibCommon::vprint(t("Node")." '".$node->title."' (NID:".$node->nid."/GID:".$object_id.") ".t("attributes processed incorrectly"), 0);
+        return NULL;
+      }
     }
   }
 
   /**
-  * Update node field
+  * Change node field
   *
   * @param &node
   *    Reference to node object
@@ -269,12 +271,158 @@ class GeslibWriter {
   * @param attr_format (optiona)
   *    Attribute format
   */
-  function update_attribute(&$node, $attr_name, $attr_value, $attr_format=NULL) {
-    $value = array('value' => $attr_value);
-    if ($attr_format) {
-      $value['format'] = $attr_format;
+  function change_attribute(&$node, $attr_name, $attr_value, $attr_format=NULL) {
+    $changed = false;
+    # El body siempre lo mapeamos igual
+    if ($attr_name == 'body') {
+      $field_name = 'body';
+    } else {
+      # Obtenemos el campo a mapear de la configuracion del modulo
+      $field_name = variable_get('geslib_'.$this->elements_type.'_attribute_'.$attr_name, NULL);
     }
-    $node->$attr_name = array('und' => array($value));
+
+    # Si existe mapeo para el campo, lo actualiza
+    if ($field_name) {
+      # Si hemos dicho que esta vinculado a una taxonomia
+      if (strpos($field_name, "#vid-") === 0) {
+        $vid = substr($field_name,5);
+        # Pide actualizar el vocabulario
+        $this->update_vocabulary_terms($node,$vid,$attr_value);
+      # El resto de atributos los actualizamos de forma normal
+      } else {
+        # Construimos un array de valores
+        $field_values = array();
+        # Si los valores ya son un array los cargaremos de ahi
+        if (is_array($attr_value)) {
+          $attr_values = $attr_value;
+        # En caso contrario, construimos un array de un solo valor
+        } else {
+          $attr_values = array($attr_value);
+        }
+        # Recorremos el array de todos los valores
+        foreach ( $attr_values as $attr_element ) {
+          # construimos el elemento
+          $field_value = array( 'value' => $attr_element );
+          if ($attr_format) {
+            $field_value['format'] = $attr_format;
+          }
+          # asignamos el valor al array de campos
+          $field_values[] = $field_value;
+        }
+        # y lo asociamos al nodo
+        $node->$field_name = array('und' => $field_values);
+      }
+      $changed = true;
+    }
+    return $changed;
+  }
+
+  /**
+  * Update vocabulary terms
+  *
+  * @node
+  *   drupal node
+  * @vid
+  *   vocabulary ID
+  * @param values
+  *   object terms separated by comma
+  */
+  function update_vocabulary_terms(&$node, $vid, $values) {
+    if ($values && $values != "") {
+      GeslibCommon::vprint(t("Updating vocabulary").": ".$values);
+      $terms = array();
+      $terms['tags'] = array($vid => $values);
+      taxonomy_node_save($node, $terms);
+      # Another way to do it
+      /*
+      foreach (explode(",", $values) as $term) {
+        $new_term = array('name' => $term, 'parent' => 0, 'vid' => $vid);
+        taxonomy_save_term($autoterm);
+      }
+      */
+    }
+  }
+
+  /**
+  * Update relationships
+  *
+  * @param node
+  *   reference to drupal node
+  * @param object
+  *   object properties
+  */
+  function update_relationships(&$node, &$object) {
+    if ($node->nid && ($relations = $object['relation'])) {
+      $updated = false;
+      # Allowed node fields for that node type
+      $allowed = field_info_instances('node', $this->node_type);
+
+      # Loop all relation array
+      foreach ( $relations as $rel_name => $rel_values ) {
+        $field_name = variable_get('geslib_'.$this->elements_type.'_link_to_'.$rel_name, NULL);
+        # If there is any relation defined and it's any of the allowed fields
+        if ($field_name && $allowed[$field_name]) {
+          GeslibCommon::vprint("Actualizando relaciones con ".$rel_name,2);
+          # Array used to store relation
+          $linked_elements = array();
+          # Loop all related values (it could be multivalued)
+          foreach ( $rel_values as $rel_element ) {
+            # If related element has node type, we can search it
+            if ( $rel_node_type = variable_get('geslib_'.$rel_name.'_node_type', NULL) ) {
+              # If relation is about authors, and link_only_authors is true, and is not an author, ignore it
+              if ( $rel_name == "author" && variable_get('geslib_book_link_only_authors', NULL) && $rel_element["function"] != "A") {
+                GeslibCommon::vprint(t("Ignoring author relationship. The type is:") . " " . $rel_element["function"]);
+              } else {
+                $linked_nid = $this->get_nid_by_gid($rel_element["gid"], $rel_name, $rel_node_type);
+                # If related object exists, link it
+                if ($linked_nid) {
+                  GeslibCommon::vprint("Guardando la referencia a ".$rel_node_type." ('".$field_name."/NID:".$linked_nid."/GID:".$rel_element["gid"].")");
+                  # If related node field is nodereference, we store only related object nid
+                  if (preg_match('#^nodereference#', $allowed[$field_name]['widget_type']) === 1) {
+                    $linked_elements[] = array( 'nid' => $linked_nid );
+                  # For text fields, store the relation title value
+                  } else {
+                    $linked = $this->get_node_by_gid($rel_element["gid"], $rel_name, $rel_node_type);
+                    $linked_elements[] = array( 'value' => $linked->title );
+                    $linked = NULL;
+                  }
+                } else {
+                  GeslibCommon::vprint("ERROR: El nodo ". $rel_node_type ." relacionado (GID:". $rel_element["gid"] .") no pudo encontrarse",0);
+                }
+              }
+            # If related element could not be found, look it in geslib array
+            } else if ($allowed[fields][$field_name][type] != "nodereference" ) {
+              $referenced_value = $this->elements[$rel_name][$rel_element["gid"]]["title"];
+              if ( $referenced_value ) {
+                $linked_elements[] = array( 'value' => $referenced_value );
+                GeslibCommon::vprint("Guardando el valor ('".$referenced_value."'/GID:".$rel_element["gid"].") como ".$field_name);
+              } else {
+                GeslibCommon::vprint("ERROR: No pudo encontrarse el valor de la referencia en el fichero geslib (".$rel_name."/GID:".$rel_element["gid"].")",0);
+              }
+            } else {
+              GeslibCommon::vprint("ERROR: No se que hacer con el elemento relacionado",0);
+            }
+          }
+          # Store relationship in associated node field
+          $node->$field_name = $linked_elements;
+          $linked_elements = NULL;
+          $updated = true;
+        }
+      }
+      # Check that node is ready to save
+      if ($updated) {
+        if ($node_to_save = node_submit($node)) {
+          node_save($node_to_save);
+          GeslibCommon::vprint(t("Node")." '".$node_to_save->title."' (NID:".$node_to_save->nid."): ".t("relationships updated correctly"),2);
+        } else {
+          #print_r($node);
+          GeslibCommon::vprint(t("Relationships for node")." '".$node->title."' (NID:".$node->nid.") ".t("not updated"), 0);
+        }
+      } else {
+        GeslibCommon::vprint(t("There is no relationships to update"));
+      }
+      $node_to_save = NULL;
+    }
   }
 
   /**
