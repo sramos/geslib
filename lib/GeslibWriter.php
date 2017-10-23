b6146da88cfb6c493a2b227623155f43f96c395a
sramos <sramos@sitiodistinto.net>
2017-10-23 20:25:32 +0200

    Trazas de memoria usada

--- lib/GeslibWriter.php
+++ lib/GeslibWriter.php
@@ -768,6 +768,6 @@ class GeslibWriter {
     GeslibCommon::vprint("\n---------------------- ".t("Flush all caches"),2);
     drupal_flush_all_caches();
     GeslibCommon::vprint("                       Removed cycles: ".gc_collect_cycles()."\n",2);
-    GeslibCommon::vprint("Memory usage: " . memory_get_usage(true), 3);
+    GeslibCommon::vprint("*** Memory usage: " . memory_get_usage(true), 3);
   }
 }
